753a0d5a5f4b21b9d02c869ac21ee6d819172383
Linghua Zhang <linghuazhang@LinghuadeMacBook-Pro.local>
2024-12-24 22:30:27 -0600

    feat: update config

--- config/train_gpt2_kaggle.py
+++ config/train_gpt2_kaggle.py
@@ -13,7 +13,7 @@ wandb_run_name= 'run' + str(time.time())
 device = 'cuda'
 
 
-data_dir = 'data/openwebtext'
+data_dir = '/kaggle/input/openwebtext'
 compile = False
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
